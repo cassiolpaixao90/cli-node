4eb2b21d2b7c64366712ff6e66f66024eba73e98
Cássio Paixão <cassiopaixaoti@gmail.com>
2018-09-25 16:48:16 -0300

    config initial

--- bin/init.js
+++ bin/init.js
@@ -7,35 +7,48 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const inquirer = require("inquirer");
+const exec = require('child_process').exec;
+const fs = require('fs');
+const {
+  prompt
+} = require('inquirer');
 
 
 clear();
 console.log(
   chalk.green(
-    figlet.textSync('', {
+    figlet.textSync('NodeJS Generator <3', {
       horizontalLayout: 'full'
     })
   )
 );
 
 program
-  .version('0.0.1')
-  .description('NodeJS generator');
+  .description('start project nodejs + express')
+
+const questions = [{
+  type: 'input',
+  name: 'name',
+  message: 'Enter name project: '
+}];
 
 program
-  .command('NodeJS generator')
-  .description('start project nodejs + express')
-  .action(function () {
-
+  .command('create')
+  .alias('c')
+  .description('Create Project')
+  .action(() => {
+    prompt(questions).then((answers) => {
+      console.log("answers", answers);
+    });
   });
 
 process.on('SIGINT', function (exit) {
 
 });
 
+
 if (!process.argv.slice(2).length || !/[arudl]/.test(process.argv.slice(2))) {
   program.outputHelp();
-  process.exit(0);
+  process.exit();
 }
-
-program.parse(process.argv);
+program.parse(process.argv)
