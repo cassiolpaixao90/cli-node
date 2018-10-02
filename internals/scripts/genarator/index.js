6543c49698361f711ba8190eba9a8ea607261ac7
Cássio Paixão <cassiopaixaoti@gmail.com>
2018-10-02 08:59:09 -0300

    create generate services

--- internals/scripts/genarator/index.js
+++ internals/scripts/genarator/index.js
@@ -1,13 +1,13 @@
   const shell = require('shelljs');
-  const ServiceBuilder = require('../../templates')
+  const serviceBuilder = require('../../templates/service')
 
   class Generator {
 
     getPath(name) {
       const ret = shell.pwd();
       const path = `${ret}\\${name}`
-      const dataF = thi.data(name, path)
-      generate();
+      const dataF = this.data(name, path)
+      this.generate(dataF);
     }
 
     data(name, path) {
@@ -17,11 +17,12 @@
       }
     }
 
-    generate() {
-      ServiceBuilder(dataF)
-        .withInit()
-        .withCreate()
-        .withEnd()
+    generate(dataF) {
+      serviceBuilder(dataF)
+        .withController()
+        .withTest()
+        .withRepository()
+        .withService()
         .build();
     }
 
--- internals/templates/service/index.js
+++ internals/templates/service/index.js
@@ -1,16 +1,16 @@
 //https: //thisdavej.com/learn-handlebars-with-node-js-and-help-freddys-fish/#custom-helper-to-provide-conditional-formatting
 const fs = require('fs');
+const {
+  resolve
+} = require('path')
 const handlebars = require('handlebars');
-const baseBuilder = require('../pattern/base-builder')
-
-const controler = require('../templates/service/controller.hbs')
+const baseBuilder = require('../../pattern/base-builder')
 
 class ServiceBuilder extends baseBuilder {
 
   constructor(data) {
     super();
     this.data = data;
-    console.log("this.data", this.data);
     this.controller = this.moduleController();
     this.test = this.moduleTest();
     this.repository = this.moduleRepository();
@@ -20,24 +20,25 @@ class ServiceBuilder extends baseBuilder {
 
   moduleController() {
     const inFileController = './controller.hbs';
+    console.log("inFileController", inFileController);
     const outFileController = './controller.js';
     this.generateFile(inFileController, outFileController);
   }
 
   moduleTest() {
-    const inFileTest = './test.hbs';
+    const inFileTest = 'test.hbs'
     const outFileTest = './test.js';
     this.generateFile(inFileTest, outFileTest);
   }
 
   moduleRepository() {
-    const inFileRepository = './repository.hbs';
+    const inFileRepository = resolve('repository.hbs')
     const outFileRepository = './repository.js';
     this.generateFile(inFileRepository, outFileRepository);
   }
 
   moduleService() {
-    const inFileService = './service.hbs';
+    const inFileService = resolve('service.hbs')
     const outFileService = './service.js';
     this.generateFile(inFileService, outFileService);
   }
